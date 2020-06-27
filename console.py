70df15355e76b8bfbd55cc63b55b6147284a8c1c
andrescallejasg <andres.callejasg@gmail.com>
2020-06-27 23:30:09 +0000

    create(done) and show(in process)

--- console.py
+++ console.py
@@ -8,6 +8,8 @@
 
 import cmd
 import models
+from models.base_model import BaseModel
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -24,6 +26,30 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         'Handle empty line, do not execute anything'
         pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel,
+saves it (to the JSON file) and prints the id
+        """
+        if args == "":
+            print("** class name missing **")
+        else:
+            try:
+                obj = eval(args)()
+                models.storage.new(obj)
+                models.storage.save()
+            except NameError:
+                print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """ Prints the string representation of an instance
+based on the class name and id
+        """
+        argv = args.split(" ")
+        if argv[0] == "":
+            print("** class name missing **")
+        elif len(argv) == 1:
+            print("** instance id missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
