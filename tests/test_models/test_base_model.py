daf44d98b6e8397a0c72760071fa27a377b9c0fb
andrescallejasg <andres.callejasg@gmail.com>
2020-06-30 23:54:12 +0000

    more tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -69,5 +69,17 @@ class TestBase_model(unittest.TestCase):
         self.my_model.save()
         self.assertNotEqual(old_updated_time, self.my_model.updated_at)
 
+    def test_init_kwargs(self):
+        """[summary]
+        """
+        obj = {"updated_at": "2020-06-30T23:36:25.091664",
+               "created_at": "2020-06-30T23:36:25.091664",
+               "__class__": "BaseModel",
+               "id": "77822a4e-7aa5-4bb9-871c-5d32f34080e0"}
+        new = BaseModel(**obj)
+        self.assertEqual(new.id, "77822a4e-7aa5-4bb9-871c-5d32f34080e0")
+        self.assertEqual(new.created_at, datetime(2020, 6, 30, 23, 36, 25, 91664))
+        self.assertEqual(new.updated_at, datetime(2020, 6, 30, 23, 36, 25, 91664))        
+
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,7 +18,7 @@ from models.place import Place
 from models.review import Review
 import pep8
 from datetime import datetime
-import os.path
+import os
 from os import path
 
 
@@ -26,13 +26,16 @@ from os import path
 class TestFile_storage(unittest.TestCase):
     """ Test cases for base_model.py
     """
-    base = BaseModel()
-    id = base.id
 
     @classmethod
-    def setUp(cls):
+    def setUp(self):
         """ Setting up before start all cases
         """
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        FileStorage._FileStorage__objects = {}
+        self.base = BaseModel()
+
 
     def tearDown(self):
         """ Executed after each test
@@ -56,51 +59,55 @@ class TestFile_storage(unittest.TestCase):
         self.assertIsNotNone(FileStorage.reload.__doc__)
         self.assertIsNotNone(FileStorage.destroy.__doc__)
 
-
     def test_init(self):
         """ correct creation if instance
         """
         self.assertIsInstance(FileStorage._FileStorage__objects, dict)
         self.assertIsInstance(FileStorage._FileStorage__file_path, str)
 
-
     def test_all(self):
         """ test of new
         """
         self.assertIsInstance(models.storage.all(), dict)
-        
+        with self.assertRaises(TypeError) as error:
+            models.storage.all(5)
+        msg = "all() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(error.exception), msg)
 
     def test_save(self):
         """
         """
         models.storage.save()
         self.assertTrue(path.exists("file.json"))
-    
+        with self.assertRaises(TypeError) as error:
+            models.storage.save(5)
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(error.exception), msg)
+
     def test_new(self):
         """[summary]
         """
         models.storage.new(self.base)
+        self.assertFalse(FileStorage._FileStorage__objects == {})    
 
     def test_reload(self):
         """[summary]
         """
-        pass
-    
+        with self.assertRaises(TypeError) as error:
+            models.storage.reload(5)
+        msg = "reload() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(error.exception), msg)
+
     def test_destroy(self):
         """[summary]
         """
-        pass
+        with self.assertRaises(TypeError) as error:
+            models.storage.destroy(5, "3")
+        msg = "destroy() takes 2 positional arguments but 3 were given"
+        self.assertEqual(str(error.exception), msg)
 
-
-
-    
-        
-
-        
-
-
-
-        
+        with self.assertRaises(KeyError) as error:
+            models.storage.destroy("32")
 
 if __name__ == '__main__':
     unittest.main()
