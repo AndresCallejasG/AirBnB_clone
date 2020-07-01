5cc9584b77f3ccd294908ab7b843a821f4744e42
Duvan <duvanjarin@gmail.com>
2020-07-01 11:34:17 -0500

    add tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -10,7 +10,7 @@ import unittest
 import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.user import User
+from models.state import State
 from datetime import datetime
 import os
 from os import path
@@ -33,24 +33,21 @@ class TestReview(unittest.TestCase):
         """ PEP 8 validation
         """
         pep8_style = pep8.StyleGuide(quiet=True)
-        pep8_result = pep8_style.check_files(['models/user.py'])
+        pep8_result = pep8_style.check_files(['models/state.py'])
         self.assertEqual(pep8_result.total_errors, 0, "fix pep8")
 
     def test_doc(self):
         """ Check for documentation
         """
-        self.assertIsNotNone(User.__doc__)
-        self.assertIsNotNone(User.__init__.__doc__)
+        self.assertIsNotNone(State.__doc__)
+        self.assertIsNotNone(State.__init__.__doc__)
 
     def test_init(self):
         """ Review all the class and BaseModel attributes
         """
-        my_class = User()
-        self.assertTrue(hasattr(my_class, "email"))
-        self.assertTrue(hasattr(my_class, "password"))
-        self.assertTrue(hasattr(my_class, "first_name"))
-        self.assertTrue(hasattr(my_class, "last_name"))
-        self.assertIsInstance(my_class, User)
+        my_class = State()
+        self.assertTrue(hasattr(my_class, "name"))
+        self.assertIsInstance(my_class, State)
         self.assertTrue(type(my_class.updated_at) is datetime)
         self.assertTrue(type(my_class.created_at) is datetime)
 
@@ -59,25 +56,19 @@ class TestReview(unittest.TestCase):
         """
         obj = {"updated_at": "2020-06-30T23:36:25.091664",
                "created_at": "2020-06-30T23:36:25.091664",
-               "__class__": "User",
+               "__class__": "State",
                "id": "77822a4e-7aa5-4bb9-871c-5d32f34080e0",
-               "email": "hbnb@holb.com",
-               "password": "12345",
-               "first_name": "Betty",
-               "last_name": "Holberton"}
-        my_class = User(**obj)
-        self.assertTrue(hasattr(my_class, "email"))
-        self.assertTrue(hasattr(my_class, "password"))
-        self.assertTrue(hasattr(my_class, "first_name"))
-        self.assertTrue(hasattr(my_class, "last_name"))
-        self.assertIsInstance(my_class, User)
+               "name": "Betty"}
+        my_class = State(**obj)
+        self.assertTrue(hasattr(my_class, "name"))
+        self.assertIsInstance(my_class, State)
         self.assertTrue(type(my_class.updated_at) is datetime)
         self.assertTrue(type(my_class.created_at) is datetime)
 
     def test_heritage(self):
         """ check it is a subclass of BaseModel
         """
-        my_class = User()
+        my_class = State()
         self.assertTrue(issubclass(type(my_class), BaseModel))
 
 
